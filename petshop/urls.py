549669a4801a164cce384ad83447991bc8581388
Keane Viktor <scren2010@gmail.com>
2020-09-02 10:06:24 +0600

    4.0

--- petshop/urls.py
+++ petshop/urls.py
@@ -20,6 +20,14 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.views import get_swagger_view
 
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from rest_auth.registration.views import SocialLoginView
+
+
+
+class GoogleLogin(SocialLoginView):
+    adapter_class = GoogleOAuth2Adapter
+
 
 API_TITLE = 'Pet shop'  # new
 API_DESCRIPTION = 'Моя документация'
@@ -47,5 +55,7 @@ urlpatterns = [
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
 
     path('swagger-docs/', schema_view),
+
+    path('rest-auth/google/', GoogleLogin.as_view(), name='google_login')
 
 ]
